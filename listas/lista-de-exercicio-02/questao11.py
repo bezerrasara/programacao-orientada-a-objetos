28e06f5f5997413dad7d2ca68858e207d8b0314b
Sara Moreira <moreirasara340@gmail.com>
2019-10-24 16:37:28 -0300

    adicionando a questão

---
+++ listas/lista-de-exercicio-02/questao11.py
@@ -0,0 +1,10 @@
+num1 = int(input ('numero inteiro: '))
+num2 = int (input ('numero inteiro: '))
+num3 = float  (input ('numero real: '))
+prod = (num1*2)*(num2/2)
+soma = (num1*3)*num3
+cubo = (num3**3)
+print (f'O produto do dobro do primeiro com metade do segundo é: {prod}')
+print (f'A soma do triplo do primeiro com o terceiro é: {soma}')
+print (f'O terceiro elevado ao cubo é: {cubo}')
+ 
