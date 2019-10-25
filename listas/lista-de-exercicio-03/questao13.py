82516af1341ca98f0de59e163117f4d4ae72e143
Sara Moreira <moreirasara340@gmail.com>
2019-10-25 18:27:36 -0300

    Adicionando a questão 13

---
+++ listas/lista-de-exercicio-03/questao13.py
@@ -0,0 +1,17 @@
+N = int (input ('Digite um numero de 1 a 7: '))
+if N == 1:
+    print ('1-Domingo')
+elif N == 2: 
+    print ('2-Segunda')
+elif N == 3:
+    print ('3-Terça')
+elif N == 4:
+    print ('4-Quarta')
+elif N == 5:
+    print ('5-Quinta')
+elif N == 6:
+    print ('6-Sexta')
+elif N == 7:
+    print ('7-Sábado')
+else:
+    print ('Número inválido.')
