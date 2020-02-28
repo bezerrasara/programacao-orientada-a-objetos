a050e0372913fb044634fbf0a627c153e6f2ca4c
Sara Moreira <moreirasara340@gmail.com>
2020-02-27 21:51:45 -0300

    prova N1.2

--- provas/oo/conta.py
+++ provas/oo/conta.py
@@ -6,6 +6,7 @@ class Historico:
         self.transacoes = []
 
     def imprime(self):
+        print('\nHistorico:')
         print(f'data de abertura: {self.data_abertura}')
         if self.transacoes: 
             print('transacoes: ')
@@ -20,7 +21,8 @@ class Cliente:
         self.sobrenome= sobrenome    
         self.cpf = cpf
     def imprime(self):
-        print(f'nome: {self.nome} {self.sobrenome}\nCPF: {self.cpf}')
+        print(f'Dados do cliente:\nnome: {self.nome} {self.sobrenome}\nCPF: {self.cpf}\n')
+
         
 class Conta:
     def __init__(self, numero, titular, saldo, limite, data_abertura):
@@ -30,7 +32,7 @@ class Conta:
         self.limite = limite
         self.data_abertura = data_abertura
         self.historico = Historico()
-        self.cliente = Cliente.imprime
+        
         
         
         
@@ -45,8 +47,10 @@ class Conta:
             self.saldo-=valor
             self.historico.transacoes.append(f"saque de: {valor}")
             
-    def extrato(self):
-        print(f'numero: {self.numero}\nsaldo: {self.saldo}\n{self.cliente()}')
+    def extrato(self,dados_cliente):
+        
+        print(f'Extrato da conta:\nnumero: {self.numero}\nsaldo: {self.saldo}\n')
+                
         self.historico.transacoes.append(f"extrato - saldo de: {self.saldo}")
         
     def transfere_para(self,conta_destino, valor):
@@ -59,21 +63,33 @@ class Conta:
             return True
     
 
-"""class Cliente(Conta):
-    def __init__(self,nome,sobrenome,cpf):
-        self.nome = nome
-        self.sobrenome= sobrenome    
-        self.cpf = cpf
 
-    #def imprime(self):
-       # print(f'nome: {self.nome} {self.sobrenome}\nCPF: {self.cpf}')"""
 
 class Data:
     def __init__(self, dia, mes, ano):
         self.dia = dia
         self.mes = mes
         self.ano = ano
-        
+
+
+def main():
+    cliente1 = Cliente('joao','azevedo','111111111-1')
+    cliente2 = Cliente('Maria','Alves','222222-2')
+    data = Data("03","09","2020")
+    conta1 = Conta('123-4',cliente1,1000,20000,data)
+    conta2 = Conta('123-5',cliente2,1030,20000,data)
+    conta1.deposita(200.0)
+    conta1.saca(120.0)
+    conta1.transfere_para(conta2, 300.0)
+    conta1.extrato(cliente1.imprime())
+    conta1.historico.imprime()
+    conta2.historico.imprime()
+    
+    
+
+if __name__ == '__main__':
+    main()
+
         
     
         
